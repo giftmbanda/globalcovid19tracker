6e42e45fc9a512a2a563ab60209cacd03ec9995d
Gift Banda <giftmbanda@gmail.com>
2020-10-10 15:52:12 +0200

    make it better

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        © Copyright <a href="https://www.giftmbanda.com/"> Gift M. Banda </a> 2020
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
